5100a5c6a180b2f4ce92e670ad06f27cb10d3bd0
cankutergen <39945796+cankutergen@users.noreply.github.com>
2018-11-26 17:48:17 +0300

    Update Matrix.java

--- src/genetic_assignment/Matrix.java
+++ src/genetic_assignment/Matrix.java
@@ -19,19 +19,7 @@ public final class Matrix {
 		
 		DoubleMatrix subs = realOutputs.sub(yPred);
 		DoubleMatrix pow2 = subs.mul(subs);
-		return Math.sqrt(pow2.mean());
-		
-		
-		// Error function mentioned in project document
-		// It takes more time to converge when number of input size increased
-//		DoubleMatrix sqrt = new DoubleMatrix(new double[realOutputs.getRows()][1]);
-//		
-//		for(int i = 0; i < realOutputs.getRows(); i++) {			
-//			sqrt.put(i, 0, Math.sqrt(pow2.get(i, 0)));
-//		}
-//		
-//		return sqrt.sum();
-		
+		return Math.sqrt(pow2.mean());	
 	}
 	
 	// Getting index of given value at given matrix
